a64a15dec3c5f65748bf9d152326b4adcf2aa928
FMurkz <feliciamurkes@gmail.com>
2025-02-02 19:57:38 +0100

    setup: Added post endpoint to receive webhooks (#3)

--- src/app/CIServer.py
+++ src/app/CIServer.py
@@ -9,6 +9,14 @@ class SimpleHandler(BaseHTTPRequestHandler):
         message = "Hello World!"
         self.wfile.write(message.encode())
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print("Received POST request with data:", post_data.decode())
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Webhook received')
+
 server = HTTPServer(('', 8008), SimpleHandler)
 print('Server running on port 8008...')
 server.serve_forever()
