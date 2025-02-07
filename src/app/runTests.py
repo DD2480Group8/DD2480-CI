b4a2c7c481a12fc81267e6bab67ca67001e9c555
FMurkz <feliciamurkes@gmail.com>
2025-02-07 21:14:42 +0100

    fix: made run_test run test on repo instead of local test (#14)

--- src/app/runTests.py
+++ src/app/runTests.py
@@ -1,11 +1,14 @@
 import subprocess
+import pathlib
 
-def run_tests():
+def run_tests(tests_path):
         """Run automated tests and return detailed success/failure output."""
         print("Running Tests")
-        result = subprocess.run(["pytest", "--tb=short", "test/"], capture_output=True, text=True)
+        result = subprocess.run(["pytest", "--tb=short", pathlib.Path.joinpath(tests_path, 'src', 'test')], capture_output=True, text=True)
         
         if result.returncode == 0:
             return "Tests Passed"
         else:
             return f"Tests failed:\n{result.stdout}\n{result.stderr}"
+
+
