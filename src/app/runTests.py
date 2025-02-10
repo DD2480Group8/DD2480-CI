9409715c6fde30258833c61e9986701adfe954ec
ismailwedihaji <ismailwedihaji@gmail.com>
2025-02-11 00:38:10 +0100

    fix: runTests function modified to add logs (#35)

--- src/app/runTests.py
+++ src/app/runTests.py
@@ -5,7 +5,8 @@ def run_tests(tests_path) -> bool:
         """Run automated tests and return detailed success/failure output."""
         print("Running Tests")
         result = subprocess.run(["pytest", "--tb=short", os.path.join(tests_path, 'src', 'test')], capture_output=True, text=True)
-        return result.returncode == 0
+        test_logs = result.stdout
+        return result.returncode == 0, test_logs
         
 
 
