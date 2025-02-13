68e139e221be2f41dc00a29fba735f64bc156cc8
melissasab <melissasaber01@gmail.com>
2025-02-13 15:19:25 +0100

    added  documentation for main (#57)

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,7 @@ from CIServer import SimpleHandler
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from CIServer import run_server
 
+"""Main entry point"""
 
 server = run_server(8008)
 server.serve_forever()
